25237f9d74dc6808daf534655addb82dedfd091d
Mrperfect <abdujabborqodirov571@gmail.com>
2024-11-07 14:22:39 -1200

    first commit

---
+++ uygavazifa8898.py
@@ -0,0 +1,50 @@
+# # 1-masala
+# products = ["olma", "banan", "non", "sut"]
+# products.append("tuxum")
+# products.append("yog'")
+# print(products)
+
+
+# 2-masalar
+# numbers = [3, 7, 2, 9, 1]
+
+# max_number = max(numbers)
+# min_number = min(numbers)
+# print("Eng katta son:", max_number)
+# print("Eng kichik son:", min_number)
+
+
+# # 3-masala
+
+# orders = ["Abdulloh: 3 ta olma", "Alisher: 2 ta nok", "nuriddin: 1 ta kola", "Dilnoza: 5 ta banan"]
+
+# customer_name = input("haridor nomini kiriting: ")
+# order_found = False
+# for order in orders:
+#     if order.startswith(customer_name):
+#         print("buyurtma:", order)
+#         order_found = True
+#         break
+# if not order_found:
+#     print("haridor topilmadi")
+
+
+# 4-masala
+# even_numbers = list(range(2, 101, 2))
+# print("1 dan 100 gacha bo'lgan juft sonlar:", even_numbers)
+
+
+# # 5-masala
+# numbers = [5, 2, 9, 1, 5, 6, 11, 10, 12]
+# numbers.sort()
+
+# n = len(numbers)
+# if n % 2 == 1:  
+#     median = numbers[n // 2]
+# else:  
+#     median = (numbers[n // 2 - 1] + numbers[n // 2]) / 2
+# print("Tartiblangan ro'yxat:", numbers)
+# print("O'rtacha qiymat:", median)
+
+
+
